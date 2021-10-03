1e7fc5a728bfdff27ad2e4f466c942746c37a002
Carlos Alonso Ferreira <CarloosAF@hotmail.es>
2021-10-03 11:03:41 +0200

    Solved enemy rotation problems

--- src/actors/enemies/test_enemy.gd
+++ src/actors/enemies/test_enemy.gd
@@ -1,22 +1,18 @@
 extends Enemy
 
 var ass = preload("res://assets/sprites/tanque_ass.png")
-var tanque = preload("res://assets/sprites/tanque.png")
+var side = preload("res://assets/sprites/tanque.png")
 var cnt = 0
 
-func _process(delta):
-	_rotate_sprite()
+func _ready():
+	get_node("../Rotate").connect("rotate", self, "change_sprite")
 
-	if cnt == 5:
-		print("Pathfollow =" + (rotation_degrees as String))
-		print("Body =" + ($Body.rotation_degrees as String))
-	cnt = (cnt + 1) % 6
+#func _fixed_process(delta):
+#	if cnt == 5:
+#		print("Pathfollow =" + (rotation_degrees as String))
+#		print("Body =" + ($Body.rotation_degrees as String))
+#	cnt = (cnt + 1) % 6
 
-func _rotate_sprite():
-	if rotation_degrees >= -180 + 45:
-		$Body/Sprite.texture = ass
-		$Body/Sprite.rotation_degrees = 90
-	else:
-		$Body/Sprite.texture = tanque
-		$Body/Sprite.rotation_degrees = 180
-		
+func change_sprite():
+	$Body/Sprite.texture = ass
+	$Body/Sprite.rotation_degrees = 90
